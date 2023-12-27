e177e46e56581390ec9537c8d6c90180fb549f19
vijay-aruchamy <vijayaruchamy2003@gmail.com>
2023-12-27 17:56:14 +0530

    First Commit

---
+++ bookmanagement/src/test/java/com/zerp/bookmanagement/BookmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zerp.bookmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
