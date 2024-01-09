4178b7e8267491ac03c50622495ba5445dcd7ca0
vijay-aruchamy <vijayaruchamy2003@gmail.com>
2024-01-09 19:18:26 +0530

    final commit

--- bookmanagement/src/main/java/com/zerp/bookmanagement/Model/Book.java
+++ bookmanagement/src/main/java/com/zerp/bookmanagement/Model/Book.java
@@ -2,10 +2,13 @@ package com.zerp.bookmanagement.Model;
 
 import java.time.LocalDate;
 
+//import org.springframework.data.annotation.Id;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+//import jakarta.persistence.Id;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
