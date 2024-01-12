543b725cbf4f87c23913b414da9afb776e8b8ad6
vijay-aruchamy <vijayaruchamy2003@gmail.com>
2024-01-12 18:34:42 +0530

    days commit

--- bookmanagement/src/main/java/com/zerp/bookmanagement/Repository/CartDetailsRepository.java
+++ bookmanagement/src/main/java/com/zerp/bookmanagement/Repository/CartDetailsRepository.java
@@ -1,8 +1,6 @@
 package com.zerp.bookmanagement.Repository;
 
 import java.util.List;
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
--- bookmanagement/src/main/java/com/zerp/bookmanagement/Repository/CartRepository.java
+++ bookmanagement/src/main/java/com/zerp/bookmanagement/Repository/CartRepository.java
@@ -1,7 +1,5 @@
 package com.zerp.bookmanagement.Repository;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
